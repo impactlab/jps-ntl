acb9133f182afacb4108894426517b8869ce3e8a
Edward <edjoesu@gmail.com>
2015-03-24 12:15:32 +0800

    minor, added a little to the mrjob example

--- model/make_labels.py
+++ model/make_labels.py
@@ -4,6 +4,8 @@ meter_to_premise_file = r'../data/meter_map_cleaned.csv'
 premise_to_flag_file = r'../data/premise_to_flag.csv'
 meter_list_file = r'../data/meter_list.csv'
 
+out_file = r'../data/meter_to_flag.csv'
+
 meter_list = []
 with open(meter_list_file,'rb') as infile:
     lines = infile.readlines()
@@ -17,7 +19,6 @@ meter_to_flag=pd.merge(premise_to_flag,meter_to_premise,how='inner',left_on='pre
 
 df = meter_to_flag[meter_to_flag['meter_id'].apply(str).isin(meter_list)]
 
-aa=3
-
+df.to_csv(out_file)
 
 
--- model/mr_example.py
+++ model/mr_example.py
@@ -1,19 +1,49 @@
 from mrjob.job import MRJob
 import numpy as np
+import datetime, time
+
+"""
+USAGE:
+
+to run locally: 
+python mr_example.py <filename> 
+
+to run on EMR: 
+-Set the instance count and other parameters in mrjob.conf , then copy to /etc/
+-Set envoroment variables $AWS_ACCESS_KEY_ID and $AWS_SECRET_ACCESS_KEY
+-then the command to test on one file is is: 
+python mr_example.py -r emr s3://jps-ami-dumps/1096444__2001-01-01T000000__2015-01-01T000000.csv 
+-to run on the whole directory run: 
+python mr_example.py -r emr s3://jps-ami-dumps/
+"""
 
 class ProfilePercentiles(MRJob):
 
     def mapper(self, _, line):
         fields = line.split(',')
         try:
-            yield fields[0], float(fields[2])
+            meas_datetime = datetime.datetime.strptime(fields[1], "%Y/%m/%d %H:%M")
+            timestamp = time.mktime(meas_datetime.timetuple())
+            yield fields[0], [float(timestamp), float(fields[2])]
         except IndexError:
             pass
 
     def reducer(self, key, values):
-        profile = np.array(list(values))
-        pct = np.percentile(profile,50)
+        readings = []
+        timestamps = []
+        for val in values:
+            readings.append(val[1])
+            timestamps.append(val[0])
+        readings = np.asarray(readings)
+        timestamps = np.asarray(timestamps)
+
+        perm = timestamps.argsort()
+        
+        readings = readings[perm]
+        timestamps = timestamps[perm]
+        
+        pct = np.percentile(readings,50)
         yield key, pct
 
 if __name__ == '__main__':
-    MRWordFrequencyCount.run()
+    ProfilePercentiles.run()
